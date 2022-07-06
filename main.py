60319613d739d9cc77e0fe2150026e9c4c9d58a7
overstac <105587254+overstac@users.noreply.github.com>
2022-07-06 18:35:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from pathlib import Path
 root_dir= Path("destination")
 
 for path in root_dir.glob("*.csv"):
-  with open(path, "wb") as file:
+  with open(path, "wb") as file:         #"wb" means write binnary
     file.write(b"")                      # delete the content of the files
   path.unlink()                          # delete the files
